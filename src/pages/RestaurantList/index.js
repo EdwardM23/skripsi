97407005ea30abef7d47ca904aa1ff0eefd25419
kellylanjaya <kellylanjaya@gmail.com>
2022-11-25 00:18:17 +0700

    update RestaurantList.js
    update RestaurantList.js

--- src/pages/RestaurantList/index.js
+++ src/pages/RestaurantList/index.js
@@ -1,9 +1,10 @@
-import {Image, StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View, ImageBackground} from 'react-native';
 import React from 'react';
 import TitleComp from '../../component/TitleComp';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
 import Header from '../../component/Header'
 import {colors} from "../../global/styles"
+import Star from '../../images/star.png'
 
 const RestauranList = () => {
   return (
@@ -30,19 +31,26 @@ const RestauranList = () => {
 
             <View style={styles.item}>
               <View style={{position: 'relative', width: '100%'}}>
-                <Image
+                <ImageBackground
                   source={{
-                    uri: 'https://awsimages.detik.net.id/community/media/visual/2020/10/20/bukan-romantis-lampu-remang-remang-di-restoran-bikin-makanan-tak-enak_169.png?w=700&q=90',
+                    uri: 'https://anakjajan.files.wordpress.com/2016/10/dscf1714.jpg?w=474&h=316',
                   }}
                   style={{height: 150, borderRadius: 10}}
-                />
-
-                <View style={styles.rating_review}>
-                  <Text>Rating / review</Text>
-                </View>
+                >
+                  <View style={{marginLeft: 10, marginRight: 10, marginTop: 10, alignItems:'flex-end'}}>
+                    <View style={styles.ratingContainer}>
+                      <Image
+                        source={Star}
+                        style={{height: 15, width: 15}}
+                      />
+                      <Text style={styles.ratingText}>3.5</Text>
+                      <Text style={styles.count}>(15)</Text>
+                    </View>
+                  </View>
+                </ImageBackground>
               </View>
 
-              <View style={{width: '100%'}}>
+              <View style={{width: '100%', marginTop: 5}}>
                 <View style={styles.row}>
                   <View style={{textAlign: 'right'}}>
                     <Text style={styles.restoFont}>
@@ -58,20 +66,20 @@ const RestauranList = () => {
                       style={{width: 9, height: 13, marginRight: 2}}
                     />
 
-                    <Text style={{fontSize: 12, color: 'black'}}>12 KM</Text>
+                    <Text style={styles.distanceFont}>12 KM</Text>
                   </View>
                 </View>
 
                 <View style={styles.row}>
                   <View>
-                    <Text style={{fontSize: 12, color: 'black'}}>
+                    <Text style={{fontSize: 14, color: colors.grey, fontWeight: '400'}}>
                       Indonesia <Text style={styles.status}>Status</Text>
                     </Text>
                   </View>
 
                   <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                    <Text style={{fontSize: 12, color: 'black'}}>
-                      Price Rp 250 000 fo tow
+                    <Text style={{fontSize: 14, color: colors.grey, fontWeight: '400'}}>
+                      Price Rp 250.000 for two
                     </Text>
                   </View>
                 </View>
@@ -115,8 +123,9 @@ const styles = StyleSheet.create({
   },
 
   status: {
-    color: 'red',
-    fontWeight: 'bold',
+    color: colors.red,
+    fontWeight: '600',
+    fontSize: 14
   },
 
   row: {
@@ -124,6 +133,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-between',
     width: '100%',
+    marginTop: 2
   },
 
   containerList: {
@@ -156,6 +166,34 @@ const styles = StyleSheet.create({
 
   restoFont: {
     fontSize: 18,
+    fontWeight: '600',
+    color: colors.grey
+  },
+
+  distanceFont: {
+    fontSize: 14,
+    fontWeight: '400',
+    color: colors.grey
+  },
+
+  ratingContainer: {
+    backgroundColor: colors.white,
+    width: 75,
+    height: 30,
+    borderRadius: 15,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+
+  ratingText: {
+    fontSize: 14,
+    fontWeight: '600',
+    color: colors.grey
+  },
+
+  count: {
+    fontSize: 12,
     fontWeight: '600',
     color: colors.grey
   }
